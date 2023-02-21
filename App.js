5133b18e2209574351c2d7188cc1d0284ee3c5bf
Omole Daniel <omoledaniel13@gmail.com>
2023-02-21 17:11:34 +0100

    created login page

--- App.js
+++ App.js
@@ -4,10 +4,22 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on the app.</Text>
-      <View style={styles.button}>
-        Click on me
+      <Text style={styles.mainText}>Login</Text>
+
+      <View style={styles.detailView}>
+        <Text style={styles.subText}>Username</Text>
+        <View style={styles.input}></View>
       </View>
+      
+      <View style={styles.detailView}>
+        <Text style={styles.subText}>Password</Text>
+        <View style={styles.input}></View>
+      </View>
+      
+      <View style={styles.button}>
+        Submit
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,9 +30,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
     paddingHorizontal: "20px",
-    paddingVertical: "20px"
+    paddingVertical: "20px",
+  },
+
+  mainText: {
+    fontWeight: "800",
+    fontSize: "30px"
+  },
+
+  detailView: {
+    width: "100%",
+    marginVertical: "15px"
+  },
+
+  subText: {
+    fontWeight: "300",
+    opacity: "0.7",
+    display: "flex",
+    justifyContent: "flex-start"
   },
 
   button: {
@@ -35,5 +63,13 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     backgroundColor: "#212121",
     cursor: "pointer"
+  },
+
+  input: {
+    borderWidth: "1px",
+    borderRadius: "5px",
+    width: "100%",
+    height: "50px",
+    marginVertical: "10px"
   }
 });
