a28107ca7949697579ac51358ab523a1f127d072
Omole Daniel <omoledaniel13@gmail.com>
2023-02-21 16:56:30 +0100

    experimented by adding a button

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on the app.</Text>
+      <View style={styles.button}>
+        Click on me
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +19,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: "20px",
+    paddingVertical: "20px"
   },
+
+  button: {
+    width: "100%",
+    height: "50px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "20px",
+    borderRadius: "10px",
+    color: "white",
+    fontWeight: "bold",
+    backgroundColor: "#212121",
+    cursor: "pointer"
+  }
 });
